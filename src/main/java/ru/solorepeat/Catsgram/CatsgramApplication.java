93b27da35f7e2aa65ce0ed94ba79c436df04e05d
Sibiryaq <4totakoeaaa@mail.ru>
2024-11-18 22:50:49 +0700

    RESTful API

--- src/main/java/ru/solorepeat/Catsgram/CatsgramApplication.java
+++ src/main/java/ru/solorepeat/Catsgram/CatsgramApplication.java
@@ -3,11 +3,29 @@ package ru.solorepeat.Catsgram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * GET - получает ресурсы
+ * POST - создает ресурс
+ * PUT - заменяет существующий данные и при их отсутствии создает новый ресурс
+ * PATCH - частичное обновление данных ресурса
+ * DELETE - удаляет ресурс
+ *
+ * HEAD - для получения заголовков ответа
+ * OPTIONS - для получения списка HTTP-методов, которые поддерживает сервер
+ *
+ * небезопасный - может изменить ресурс - PUT, PATCH, DELETE, POST
+ * идемпотентный - многократное выполнение = однократному
+ * неидемпотентный - POST
+ *
+ *
+ */
 @SpringBootApplication
 public class CatsgramApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatsgramApplication.class, args);
+
+
 	}
 
 }
